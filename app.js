f1e951c13b5775ad8223938af6461ae8895ebccb
james gill <jimmygill28@hotmail.com>
2020-08-28 13:06:15 +0100

    small addition

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const {
 
 app.use(express.static(path.join(__dirname, "build")));
 
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 app.use(express.json());
 
 app.use("/api", apiRouter);
